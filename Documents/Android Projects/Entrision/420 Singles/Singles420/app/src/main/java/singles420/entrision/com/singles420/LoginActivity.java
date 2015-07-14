b7676cd45c7facc22d9398878032c30864317043
TravisKWade <travis@entrision.com>
2015-07-14 12:48:08 -0700

    - added facebook validation - added mask for sign up dob

--- Documents/Android Projects/Entrision/420 Singles/Singles420/app/src/main/java/singles420/entrision/com/singles420/LoginActivity.java
+++ Documents/Android Projects/Entrision/420 Singles/Singles420/app/src/main/java/singles420/entrision/com/singles420/LoginActivity.java
@@ -5,9 +5,17 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
@@ -19,20 +27,51 @@ import java.net.HttpURLConnection;
 
 public class LoginActivity extends Activity {
 
+    CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
 
         Utilities.UserUtility userUtil = new Utilities.UserUtility();
-
         String token = userUtil.getAuthorizationToken(this);
 
         if (token != "") {
             Intent myIntent = new Intent(this, SinglesActivity.class);
             //myIntent.putExtra("key", value); //Optional parameters
             this.startActivity(myIntent);
+        } else {
+            callbackManager = CallbackManager.Factory.create();
+
+            LoginButton loginButton = (LoginButton) this.findViewById(R.id.facebook_login_button);
+            loginButton.setReadPermissions("user_friends");
+
+            LoginManager.getInstance().registerCallback(callbackManager,
+                    new FacebookCallback<LoginResult>() {
+                        @Override
+                        public void onSuccess(LoginResult loginResult) {
+                           loginFaceBookUser(loginResult.getAccessToken().getUserId());
+                        }
+
+                        @Override
+                        public void onCancel() {
+                            // App code
+                        }
+
+                        @Override
+                        public void onError(FacebookException exception) {
+                            // App code
+                        }
+                    });
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void loginButtonPressed(View view) {
@@ -133,12 +172,59 @@ public class LoginActivity extends Activity {
             //myIntent.putExtra("key", value); //Optional parameters
             this.startActivity(myIntent);
         } catch (Exception e) {
-            System.out.println("ERROR - " + e);
+            Log.w("**** 420 SINGLES ****", e);
         }
     }
 
     public void joinButtonPressed(View view) {
         Intent myIntent = new Intent(this, SignupActivity.class);
         this.startActivity(myIntent);
+    }
+
+    public void loginFaceBookUser(String token) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        try {
+            JSONObject jsonParams = new JSONObject();
+            JSONObject fbParams = new JSONObject();
+            fbParams.put("facebook_auth_token", token);
+            jsonParams.put("user", fbParams);
+
+
+            ByteArrayEntity entity = new ByteArrayEntity(jsonParams.toString().getBytes("UTF-8"));
+            client.post(this, this.getResources().getString(R.string.base_address) + "facebook_authentication", entity, "application/json",new AsyncHttpResponseHandler() {
+
+                @Override
+                public void onStart() {
+                    // called before request is started
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                    if (statusCode == HttpURLConnection.HTTP_OK) {
+                        try {
+                            String jsonString = new String(response, "UTF-8");
+                            setUserInfo(jsonString);
+
+                        } catch(Exception e) {
+
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                    // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    Log.w("**** 420 SINGLES ****", e);
+                }
+
+                @Override
+                public void onRetry(int retryNo) {
+                    // called when request is retried
+                }
+            });
+
+        } catch (Exception e) {
+            Log.w("**** 420 SINGLES ****", e);
+        }
     }
 }
--- Documents/Android Projects/Entrision/420 Singles/Singles420/app/src/main/java/singles420/entrision/com/singles420/SignupActivity.java
+++ Documents/Android Projects/Entrision/420 Singles/Singles420/app/src/main/java/singles420/entrision/com/singles420/SignupActivity.java
@@ -1,21 +1,139 @@
 package singles420.entrision.com.singles420;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-/**
- * Created by travis on 7/7/15.
- */
+import java.util.Calendar;
+
 public class SignupActivity extends Activity {
+
+    EditText birthDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        TextWatcher tw = new TextWatcher() {
+            private String current = "";
+            private String ddmmyyyy = "DDMMYYYY";
+            private Calendar cal = Calendar.getInstance();
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]", "");
+                    String cleanC = current.replaceAll("[^\\d.]", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i = 2; i <= cl && i < 6; i += 2) {
+                        sel++;
+                    }
+                    //Fix for pressing delete next to a forward slash
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8){
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    }else{
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        int day  = Integer.parseInt(clean.substring(0,2));
+                        int mon  = Integer.parseInt(clean.substring(2,4));
+                        int year = Integer.parseInt(clean.substring(4,8));
+
+                        if(mon > 12) mon = 12;
+                        cal.set(Calendar.MONTH, mon-1);
+                        year = (year<1900)?1900:(year>2100)?2100:year;
+                        cal.set(Calendar.YEAR, year);
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, date e.g. 29/02/2012
+                        //would be automatically corrected to 28/02/2012
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                        clean = String.format("%02d%02d%02d",day, mon, year);
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                            clean.substring(2, 4),
+                            clean.substring(4, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    birthDate.setText(current);
+                    birthDate.setSelection(sel < current.length() ? sel : current.length());
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        };
+
+        birthDate = (EditText) findViewById(R.id.dob);
+        birthDate.addTextChangedListener(tw);
+
     }
 
     public void signupButtonPressed(View view) {
+        Boolean inputError = false;
+        String errorMessage = "";
+        EditText firstName = (EditText) findViewById(R.id.first_name);
+        EditText lastName = (EditText) findViewById(R.id.last_name);
+        EditText zipcode = (EditText) findViewById(R.id.zipcode);
+        //EditText birthDate = (EditText) findViewById(R.id.dob);
+        EditText emailAddress = (EditText) findViewById(R.id.email_address);
+        EditText password = (EditText) findViewById(R.id.password);
+        EditText confirmPassword = (EditText) findViewById(R.id.confirm_password);
+        Spinner gender = (Spinner) findViewById(R.id.gender);
 
+        if (firstName.getText().toString().matches("")) {
+            inputError = true;
+        }
+
+        if (lastName.getText().toString().matches("")) {
+            inputError = true;
+        }
+
+        if (emailAddress.getText().toString().matches("")) {
+            inputError = true;
+        }
+
+        if (password.getText().toString().matches("")) {
+            inputError = true;
+        }
+
+        if (!inputError) {
+
+        } else {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+            // set title
+            alertDialogBuilder.setTitle("Input Error");
+
+            // set dialog message
+            alertDialogBuilder
+                    .setMessage(errorMessage)
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
     }
+
+
 }

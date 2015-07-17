e8e2ffa4b12bd682b274841c473315eebfa9efad
TravisKWade <travis@entrision.com>
2015-07-17 08:58:33 -0700

    - call to get suggestions

--- Documents/Android Projects/Entrision/420 Singles/Singles420/app/src/main/java/singles420/entrision/com/singles420/APIHelper.java
+++ Documents/Android Projects/Entrision/420 Singles/Singles420/app/src/main/java/singles420/entrision/com/singles420/APIHelper.java
@@ -20,17 +20,16 @@ import java.net.HttpURLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class APIHelper  {
+public class APIHelper {
     private static APIHelper mInstance = null;
     private Context context;
 
-    private APIHelper(){
+    private APIHelper() {
 
     }
 
-    public static APIHelper getInstance(){
-        if(mInstance == null)
-        {
+    public static APIHelper getInstance() {
+        if (mInstance == null) {
             mInstance = new APIHelper();
         }
         return mInstance;
@@ -40,7 +39,7 @@ public class APIHelper  {
         context = c;
     }
 
-    public void loginUser (String emailAddress, String password) {
+    public void loginUser(String emailAddress, String password) {
         AsyncHttpClient client = new AsyncHttpClient();
         try {
             JSONObject jsonParams = new JSONObject();
@@ -51,7 +50,7 @@ public class APIHelper  {
 
 
             ByteArrayEntity entity = new ByteArrayEntity(jsonParams.toString().getBytes("UTF-8"));
-            client.post(context, context.getResources().getString(R.string.base_address) + "sessions", entity, "application/json",new AsyncHttpResponseHandler() {
+            client.post(context, context.getResources().getString(R.string.base_address) + "sessions", entity, "application/json", new AsyncHttpResponseHandler() {
 
                 @Override
                 public void onStart() {
@@ -69,7 +68,7 @@ public class APIHelper  {
                             Intent intent = new Intent(context, SinglesActivity.class);
                             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             context.startActivity(intent);
-                        } catch(Exception e) {
+                        } catch (Exception e) {
 
                         }
                     }
@@ -121,7 +120,7 @@ public class APIHelper  {
 
 
             ByteArrayEntity entity = new ByteArrayEntity(jsonParams.toString().getBytes("UTF-8"));
-            client.post(context, context.getResources().getString(R.string.base_address) + "users", entity, "application/json",new AsyncHttpResponseHandler() {
+            client.post(context, context.getResources().getString(R.string.base_address) + "users", entity, "application/json", new AsyncHttpResponseHandler() {
 
                 @Override
                 public void onStart() {
@@ -138,7 +137,7 @@ public class APIHelper  {
                         Intent intent = new Intent(context, SinglesActivity.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         context.startActivity(intent);
-                    } catch(Exception e) {
+                    } catch (Exception e) {
 
                     }
 
@@ -172,7 +171,7 @@ public class APIHelper  {
 
 
             ByteArrayEntity entity = new ByteArrayEntity(jsonParams.toString().getBytes("UTF-8"));
-            client.post(context, context.getResources().getString(R.string.base_address) + "facebook_authentication", entity, "application/json",new AsyncHttpResponseHandler() {
+            client.post(context, context.getResources().getString(R.string.base_address) + "facebook_authentication", entity, "application/json", new AsyncHttpResponseHandler() {
 
                 @Override
                 public void onStart() {
@@ -190,7 +189,7 @@ public class APIHelper  {
                             Intent intent = new Intent(context, SinglesActivity.class);
                             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             context.startActivity(intent);
-                        } catch(Exception e) {
+                        } catch (Exception e) {
 
                         }
                     }
@@ -199,14 +198,14 @@ public class APIHelper  {
                 @Override
                 public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
                     // the user is not known to the database, so we sign them up using facebook data
-                    new GraphRequest( facebookInfo.getAccessToken(), "/me", null, HttpMethod.GET,
+                    new GraphRequest(facebookInfo.getAccessToken(), "/me", null, HttpMethod.GET,
                             new GraphRequest.Callback() {
                                 public void onCompleted(GraphResponse response) {
                                     Log.d("*** 420 **** ", response.toString());
                                     try {
                                         JSONObject facebookObject = response.getJSONObject();
                                         APIHelper.getInstance().signUpUser(facebookObject.getString("first_name"), facebookObject.getString("last_name"), facebookObject.getString("gender"), "", facebookObject.getString("email"), facebookInfo.getAccessToken().getUserId(), facebookInfo.getAccessToken().getUserId());
-                                    } catch(Exception e) {
+                                    } catch (Exception e) {
 
                                     }
                                 }
@@ -224,4 +223,54 @@ public class APIHelper  {
             Log.w("**** 420 SINGLES ****", e);
         }
     }
+
+    public String getSuggestionsForUser() {
+        String suggestions = "";
+        Utilities.UserUtility userUtility = new Utilities.UserUtility();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.addHeader("Authorization", "Token token="+userUtility.getAuthorizationToken(context));
+        client.addHeader("X-User-Email", userUtility.getUserEmail(context));
+
+        String url = context.getResources().getString(R.string.base_address) + "suggestions";
+        try {
+
+            client.get(context.getResources().getString(R.string.base_address) + "suggestions", new AsyncHttpResponseHandler() {
+
+                @Override
+                public void onStart() {
+                    // called before request is started
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                    try {
+                        String jsonString = new String(response, "UTF-8");
+                        Log.d("*** 420 ******", jsonString);
+                    } catch (Exception e) {
+
+                    }
+
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                    // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    Log.w("***** 420 *****", e.toString());
+                }
+
+                @Override
+                public void onRetry(int retryNo) {
+                    // called when request is retried
+                }
+            });
+
+
+        } catch (Exception e) {
+
+        }
+
+        return suggestions;
+    }
+
 }
--- Documents/Android Projects/Entrision/420 Singles/Singles420/app/src/main/java/singles420/entrision/com/singles420/Utilities.java
+++ Documents/Android Projects/Entrision/420 Singles/Singles420/app/src/main/java/singles420/entrision/com/singles420/Utilities.java
@@ -10,18 +10,14 @@ import org.json.JSONObject;
 public class Utilities {
 
     public static class UserUtility {
+
+        /*** SET METHODS ****/
+
         public void setAuthorizationToken (String token, Context context){
             SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.prefsFile), Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString("authToken", token);
             editor.commit();
-        }
-
-        public String getAuthorizationToken (Context context) {
-            SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.prefsFile), Context.MODE_PRIVATE);
-            String token = prefs.getString("authToken", "");
-
-            return token;
         }
 
         public void setUserID(Integer id, Context context) {
@@ -31,11 +27,11 @@ public class Utilities {
             editor.commit();
         }
 
-        public Integer getUserID(Context context) {
+        public void setUserEmail(String email, Context context) {
             SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.prefsFile), Context.MODE_PRIVATE);
-            Integer userID = prefs.getInt("userID", -1);
-
-            return userID;
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("email", email);
+            editor.commit();
         }
 
         public void setUserInfo(Context context, String jsonString) {
@@ -44,13 +40,38 @@ public class Utilities {
                 JSONObject userObject = jsonObject.getJSONObject("user");
                 Integer userID = (Integer) userObject.get("id");
                 String token = (String) userObject.get("authentication_token");
+                String email = (String) userObject.get("email");
 
                 setUserID(userID, context);
                 setAuthorizationToken(token, context);
+                setUserEmail(email, context);
 
             } catch (Exception e) {
                 Log.w("**** 420 SINGLES ****", e);
             }
+        }
+
+        /*** GET METHODS ****/
+
+        public String getAuthorizationToken (Context context) {
+            SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.prefsFile), Context.MODE_PRIVATE);
+            String token = prefs.getString("authToken", "");
+
+            return token;
+        }
+
+        public Integer getUserID(Context context) {
+            SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.prefsFile), Context.MODE_PRIVATE);
+            Integer userID = prefs.getInt("userID", -1);
+
+            return userID;
+        }
+
+        public String getUserEmail(Context context) {
+            SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.prefsFile), Context.MODE_PRIVATE);
+            String userEmail = prefs.getString("email", "");
+
+            return userEmail;
         }
     }
 }

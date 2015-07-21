b7ade3329c23416460a95b4275ac18a4ea244cc6
TravisKWade <travis@entrision.com>
2015-07-21 10:40:07 -0700

    - yes, no, info buttons added

---
+++ Documents/Android Projects/Entrision/420 Singles/Singles420/app/src/main/java/singles420/entrision/com/singles420/SuggestionViewFragment.java
@@ -0,0 +1,19 @@
+package singles420.entrision.com.singles420;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by travis on 7/20/15.
+ */
+public class SuggestionViewFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_suggestion_view, container, false);
+    }
+}

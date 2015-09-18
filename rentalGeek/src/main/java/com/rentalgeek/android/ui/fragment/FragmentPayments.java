c47338cee3c3f031d803b300dca219d762f7f511
Charles Deisler <c33jd13s3l@gmail.com>
2015-09-18 14:05:58 -0400

    Remove keylistene

--- rentalGeek/src/main/java/com/rentalgeek/android/ui/fragment/FragmentPayments.java
+++ rentalGeek/src/main/java/com/rentalgeek/android/ui/fragment/FragmentPayments.java
@@ -103,7 +103,7 @@ public class FragmentPayments extends GeekBaseFragment implements Validator.Vali
 
         evaluatePendingPayments();
 
-        KeyListener();
+        //KeyListener();
 
         return v;
     }

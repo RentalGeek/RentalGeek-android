190e477b72f00eb4ebf768eee32bf4686acdb6ab
Adam Johns <robert.adam.johns@gmail.com>
2015-09-19 12:57:29 -0500

    handling loading for cosigner call if don't already have page destination before tapping cosigner menu item

--- rentalGeek/src/main/java/com/rentalgeek/android/ui/activity/ActivityCosignDecider.java
+++ rentalGeek/src/main/java/com/rentalgeek/android/ui/activity/ActivityCosignDecider.java
@@ -1,21 +1,39 @@
 package com.rentalgeek.android.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 
+import com.rentalgeek.android.R;
 import com.rentalgeek.android.utils.CosignerInviteCaller;
 
 /**
  * Created by rajohns on 9/16/15.
  *
  */
-public class ActivityCosignDecider extends Activity {
+public class ActivityCosignDecider extends GeekBaseActivity {
+
+    public ActivityCosignDecider() {
+        super(true, true, true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_with_fragment);
+
+        setupNavigation();
+
         new CosignerInviteCaller(this, true).fetchCosignerInvites();
-        finish();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showProgressDialog(R.string.dialog_msg_loading);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        hideProgressDialog();
+    }
 }

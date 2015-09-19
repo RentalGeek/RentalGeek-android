99fd89cde8bc1475f35cee9f2cb91d050b8cdd97
Adam Johns <robert.adam.johns@gmail.com>
2015-09-19 13:25:58 -0500

    starting cosigner app screen

--- rentalGeek/src/main/java/com/rentalgeek/android/utils/CosignerDestinationLogic.java
+++ rentalGeek/src/main/java/com/rentalgeek/android/utils/CosignerDestinationLogic.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 
 import com.rentalgeek.android.api.SessionManager;
 import com.rentalgeek.android.pojos.CosignerInviteDTO;
+import com.rentalgeek.android.ui.activity.ActivityCosignerApp;
 import com.rentalgeek.android.ui.activity.ActivityCosignerInvite;
 import com.rentalgeek.android.ui.activity.ActivityCosignerList;
 
@@ -95,7 +96,7 @@ public enum CosignerDestinationLogic {
         if (hasOutstandingInvites()) {
             activity.startActivity(new Intent(activity, ActivityCosignerInvite.class));
         } else if (!hasCompletedCosignerProfile()) {
-            // navigate to cosigner app
+            activity.startActivity(new Intent(activity, ActivityCosignerApp.class));
         } else {
             activity.startActivity(new Intent(activity, ActivityCosignerList.class));
         }

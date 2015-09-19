d5ca1bb154b2f0d11269e260a37b84c8e409706f
Adam Johns <robert.adam.johns@gmail.com>
2015-09-19 15:37:48 -0500

    ui work for cosigner app

--- rentalGeek/src/main/java/com/rentalgeek/android/ui/fragment/FragmentCosignerApp.java
+++ rentalGeek/src/main/java/com/rentalgeek/android/ui/fragment/FragmentCosignerApp.java
@@ -4,20 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.rentalgeek.android.R;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class FragmentCosignerApp extends GeekBaseFragment {
+
+	@InjectView(R.id.first_name_edittext) EditText firstNameEditText;
+	@InjectView(R.id.last_name_edittext) EditText lastNameEditText;
+	@InjectView(R.id.month_spinner) Spinner monthSpinner;
+	@InjectView(R.id.day_spinner) Spinner daySpinner;
+	@InjectView(R.id.year_edittext) EditText yearEditText;
 
     @Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_cosigner_app, container, false);
 
-		ButterKnife.inject(this,view);
+		ButterKnife.inject(this, view);
         
 		return view;
+	}
+
+	@OnClick(R.id.next_button)
+	public void nextButtonTapped() {
+		verifyInput();
+	}
+
+	private void verifyInput() {
+
 	}
 
 }

1868a0e431a44fea14209c9e146b52c995b90b20
Alan Ruvalcaba <aruval3@gmail.com>
2015-09-29 02:07:00 -0400

    Remove pager bar

--- rentalGeek/src/main/java/com/rentalgeek/android/ui/activity/ActivityCreateProfile.java
+++ rentalGeek/src/main/java/com/rentalgeek/android/ui/activity/ActivityCreateProfile.java
@@ -12,8 +12,6 @@ import com.mobsandgeeks.saripaar.Validator;
 import com.rentalgeek.android.R;
 import com.rentalgeek.android.ui.adapter.ProfileFormAdapter;
 import com.rentalgeek.android.ui.view.NonSwipeableViewPager;
-import com.viewpagerindicator.CirclePageIndicator;
-import com.viewpagerindicator.PageIndicator;
 
 public class ActivityCreateProfile extends GeekBaseActivity implements Validator.ValidationListener {
 
